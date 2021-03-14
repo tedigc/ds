503cfa537108a88ce537e328c3f59d53785e116f
ted <ted.cater@acresoftware.com>
2021-03-14 15:59:52 +0000

    add button variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 type Props = {
   children: React.ReactNode
+  variant: 'primary' | 'secondary'
 }
 
-const Button = ({ children, ...rest }: Props) => {
-  return <button {...rest} className="px-5 py-2 bg-red-400 text-white tracking-wide font-semibold rounded-md">{children}</button>
+const variants = {
+  'primary': 'transition-all px-5 py-2 bg-red-400 text-white tracking-wide font-semibold rounded-md hover:bg-red-500 border-solid rounded-md border-2 border-red-400 hover:border-red-500 hover:text-red-500',
+  'secondary': 'transition-all px-5 py-2 bg-white text-red-400 tracking-wide font-semibold border-solid rounded-md border-2 border-red-400 hover:border-red-500 hover:text-red-500'
+}
+
+const Button = ({ children, variant, ...rest }: Props) => {
+  return <button {...rest} className={variants[variant]}>{children}</button>
 }
 
 export default Button
--- src/pages/components/button.tsx
+++ src/pages/components/button.tsx
@@ -6,7 +6,9 @@ const Page = () => {
   return (
     <Layout>
       <h1 className="text-2xl font-bold text-gray-800 mb-4">Button</h1>
-      <Button>Click me!</Button>
+
+      <div className="mb-4"><Button variant="primary">Primary button</Button></div>
+      <Button variant="secondary">Secondary button</Button>
     </Layout>
   )
 }
