23fe937003ef3cce3f6bdf1e4096f60128c5f1a1
tedigc <tedigc@gmail.com>
2021-03-31 22:50:01 +0100

    add basic prefixes and suffixes

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLProps } from "react";
+import React, { HTMLProps, PropsWithChildren } from "react";
 import classnames from "classnames";
 
 type InputProps = {
@@ -10,4 +10,16 @@ export const Input = (props: HTMLProps<HTMLInputElement> & InputProps) => {
   return (
     <input {...rest} className={classnames("input", error && "input-error")} />
   );
+};
+
+export const InputGroup = ({ children }: PropsWithChildren<any>) => {
+  return <div className="input-group">{children}</div>;
+};
+
+export const InputPrefix = ({ children }: PropsWithChildren<any>) => {
+  return <div className="flex flex-col">{children}</div>;
+};
+
+export const InputSuffix = ({ children }: PropsWithChildren<any>) => {
+  return <div className="">{children}</div>;
 };
--- src/pages/components/input.tsx
+++ src/pages/components/input.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Head from "next/head";
 import { Input } from "../../components";
+import {
+  InputGroup,
+  InputPrefix,
+  InputSuffix,
+} from "../../components/Input/Input";
 
 const Page = () => {
   return (
@@ -28,6 +33,25 @@ const Page = () => {
       <h2 className="text-lg font-bold text-gray-800 mb-2 flex">Disabled</h2>
       <div className="flex mb-5">
         <Input disabled value="Can't type here" />
+      </div>
+
+      <h2 className="text-lg font-bold text-gray-800 mb-2 flex">With prefix</h2>
+      <div className="flex mb-5">
+        <InputGroup>
+          <InputPrefix>£</InputPrefix>
+          <Input placeholder="0.00" />
+        </InputGroup>
+      </div>
+
+      <h2 className="text-lg font-bold text-gray-800 mb-2 flex">
+        With prefix and suffix
+      </h2>
+      <div className="flex mb-5">
+        <InputGroup>
+          <InputPrefix>£</InputPrefix>
+          <Input />
+          <InputSuffix>.00</InputSuffix>
+        </InputGroup>
       </div>
     </>
   );
