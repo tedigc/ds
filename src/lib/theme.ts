b074476173cf76100ce6360efe185824fda61105
tedigc <tedigc@gmail.com>
2021-04-03 18:11:47 +0100

    add theming example

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -14,25 +14,66 @@ export type Theme = {
   "primary-900": string;
 };
 
-export const theme: Theme = {
-  "primary-50": "#ecfdf5",
-  "primary-100": "#d1fae5",
-  "primary-200": "#a7f3d0",
-  "primary-300": "#6ee7b7",
-  "primary-400": "#34d399",
-  "primary-500": "#10b981",
-  "primary-600": "#059669",
-  "primary-700": "#047857",
-  "primary-800": "#065f46",
-  "primary-900": "#064e3b",
-};
-
 export const useTheme = (theme: Theme) => {
   useEffect(() => {
-    Object.keys(theme).forEach((key) => {
-      const name = `--ds-${key}`;
-      const colour = theme[key];
-      document.documentElement.style.setProperty(name, colour);
-    });
+    if (theme) {
+      console.log("changing theme...");
+      Object.keys(theme).forEach((key) => {
+        const name = `--ds-${key}`;
+        const colour = theme[key];
+        document.documentElement.style.setProperty(name, colour);
+      });
+    }
   }, [theme]);
+};
+
+export const themes = {
+  default: {
+    "primary-50": "",
+    "primary-100": "",
+    "primary-200": "",
+    "primary-300": "",
+    "primary-400": "",
+    "primary-500": "",
+    "primary-600": "",
+    "primary-700": "",
+    "primary-800": "",
+    "primary-900": "",
+  },
+  cool: {
+    "primary-50": "#EFF6FF",
+    "primary-100": "#DBEAFE",
+    "primary-200": "#BFDBFE",
+    "primary-300": "#93C5FD",
+    "primary-400": "#60A5FA",
+    "primary-500": "#3B82F6",
+    "primary-600": "#2563EB",
+    "primary-700": "#1D4ED8",
+    "primary-800": "#1E40AF",
+    "primary-900": "#1E3A8A",
+  },
+  burnt: {
+    "primary-50": "#FFFBEB",
+    "primary-100": "#FEF3C7",
+    "primary-200": "#FDE68A",
+    "primary-300": "#FCD34D",
+    "primary-400": "#FBBF24",
+    "primary-500": "#F59E0B",
+    "primary-600": "#D97706",
+    "primary-700": "#B45309",
+    "primary-800": "#92400E",
+    "primary-900": "#78350F",
+  },
+  candy: {
+    "primary-50": "#FDF2F8",
+    "primary-100": "#FCE7F3",
+    "primary-200": "#FBCFE8",
+    "primary-300": "#F9A8D4",
+    "primary-400": "#F472B6",
+    "primary-500": "#EC4899",
+    "primary-600": "#DB2777",
+    "primary-700": "#BE185D",
+    "primary-800": "#9D174D",
+    "primary-900": "#831843",
+  },
 };
--- src/pages/theming.tsx
+++ src/pages/theming.tsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "../components";
+import { useTheme, themes } from "../lib/theme";
 
 const Page = () => {
-  return <div>Theming</div>;
+  const [theme, setTheme] = useState(null);
+  useTheme(themes[theme]);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">Theming</h1>
+
+      <p className="mb-4">Click one of the buttons below to change the theme</p>
+      <ul className="flex gap-5 mb-6">
+        <li>
+          <Button
+            variant={theme == "default" ? "primary" : "secondary"}
+            onClick={() => setTheme("default")}
+          >
+            Default
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={theme == "cool" ? "primary" : "secondary"}
+            onClick={() => setTheme("cool")}
+          >
+            Cool
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={theme == "burnt" ? "primary" : "secondary"}
+            onClick={() => setTheme("burnt")}
+          >
+            Burnt
+          </Button>
+        </li>
+        <li>
+          <Button
+            variant={theme == "candy" ? "primary" : "secondary"}
+            onClick={() => setTheme("candy")}
+          >
+            Candy
+          </Button>
+        </li>
+      </ul>
+
+      <p className="mb-2">
+        Defining your own theme is as simple as passing an object into the{" "}
+        <code>useTheme</code> hook;
+      </p>
+      <pre className="bg-gray-200 px-6 rounded text-sm text-gray-700 mb-6">{`
+export const theme: Theme = {
+  "primary-50": "#FFFBEB",
+  "primary-100": "#FEF3C7",
+  "primary-200": "#FDE68A",
+  "primary-300": "#FCD34D",
+  "primary-400": "#FBBF24",
+  "primary-500": "#F59E0B",
+  "primary-600": "#D97706",
+  "primary-700": "#B45309",
+  "primary-800": "#92400E",
+  "primary-900": "#78350F",
+}
+
+// ...
+
+const App = () => {
+  useTheme(theme)
+
+  return (
+    <div>
+      {/* Your app here... */}
+    </div>
+  )
+}
+      `}</pre>
+    </div>
+  );
 };
 
 export default Page;
